725ae34935e5860c2a6cc2cbd21bab749f6cc60d
Mohit Tomer <mstomer31@gmail.com>
2023-12-17 20:27:51 +0530

    Initial Commit

---
+++ BarcodeScannerApp/BarcodeScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerAppApp.swift
+//  BarcodeScannerApp
+//
+//  Created by Mohit Tomer on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScannerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeScannerApp
+//
+//  Created by Mohit Tomer on 17/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
